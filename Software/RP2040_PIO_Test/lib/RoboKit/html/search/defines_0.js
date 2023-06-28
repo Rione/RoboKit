6c616d4377c51f06a58a21d8cea3b60d6d0f1003
tomixrm <chocolate0785lego@icloud.com>
2023-06-28 14:43:15 +0900

    :memo: update doxygen
    # ==== Emojis ====
    # ✨ :sparkles: Add new feature
    # ✅  :white_check_mark: Update existing feature (not bug)
    # ⚠️  :warning: Fix bug/error
    # 🔥  :fire: Remove files or features
    # ✏️ :pencil2: Fix typos
    # 📝  :memo: Update documentation
    # ♻️  :recycle: Dependencies update
    # 🐧  :penguin: Improved the Linux
    # 🍎  :apple: Improved the macOS
    # 🔖 :bookmark: Add new library
    # 🎉  :tada: Initial commit
    
    # ==== Format ====
    # :emoji: Subject
    #
    # Commit body...
    
    # ==== Messages ====
    # 新規(ファイル)機能追加: Add
    # 機能修正、既存機能追加（バグではない): Update
    # リファクタリング: Clean
    # ファイル名変更:
    # 削除など: Remove
    # バグ修正: Fix
    # バージョンアップ: Upgrade

---
+++ Software/RP2040_PIO_Test/lib/RoboKit/html/search/defines_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['wait_0',['wait',['../_robo_kit_8hpp.html#a80a189eed51a8d3b45cbe4cfb7c63293',1,'RoboKit.hpp']]],
+  ['wait_5fms_1',['wait_ms',['../_robo_kit_8hpp.html#af06e22dde09b08a48a54382d985d246e',1,'RoboKit.hpp']]]
+];
--- Software/RP2040_PIO_Test/lib/RoboKit/html/search/searchdata.js
+++ Software/RP2040_PIO_Test/lib/RoboKit/html/search/searchdata.js
@@ -1,10 +1,11 @@
 var indexSectionsWithContent =
 {
-  0: "bdgilmprst",
+  0: "bdgilmprstw",
   1: "dlrs",
   2: "r",
   3: "gimprs",
-  4: "blst"
+  4: "blst",
+  5: "w"
 };
 
 var indexSectionNames =
@@ -13,7 +14,8 @@ var indexSectionNames =
   1: "classes",
   2: "files",
   3: "functions",
-  4: "variables"
+  4: "variables",
+  5: "defines"
 };
 
 var indexSectionLabels =
@@ -22,6 +24,7 @@ var indexSectionLabels =
   1: "Classes",
   2: "Files",
   3: "Functions",
-  4: "Variables"
+  4: "Variables",
+  5: "Macros"
 };
 
