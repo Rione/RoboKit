972ef062bf7a4800a24ac12eb5006137c95b529d
tomixrm <chocolate0785lego@icloud.com>
2023-06-28 19:52:30 +0900

    :sparkles: Add OLED LCD with U8g2_Arduino
    add https://github.com/olikraus/U8g2_Arduino library 
    # ==== Emojis ====
    # ✨ :sparkles: Add new feature
    # ✅  :white_check_mark: Update existing feature (not bug)
    # ⚠️  :warning: Fix bug/error
    # 🔥  :fire: Remove files or features
    # ✏️ :pencil2: Fix typos
    # 📝  :memo: Update documentation
    # ♻️  :recycle: Dependencies update
    # 🐧  :penguin: Improved the Linux
    # 🍎  :apple: Improved the macOS
    # 🔖 :bookmark: Add new library
    # 🎉  :tada: Initial commit
    
    # ==== Format ====
    # :emoji: Subject
    #
    # Commit body...
    
    # ==== Messages ====
    # 新規(ファイル)機能追加: Add
    # 機能修正、既存機能追加（バグではない): Update
    # リファクタリング: Clean
    # ファイル名変更:
    # 削除など: Remove
    # バグ修正: Fix
    # バージョンアップ: Upgrade

--- Software/RP2040_PIO_Test/src/main.cpp
+++ Software/RP2040_PIO_Test/src/main.cpp
@@ -1,12 +1,20 @@
 #include <RoboKit.hpp>
 
+#include <U8g2lib.h>
+
+U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R1);
+
 void setup() {
     robot.init();
+
+    u8g2.begin(); // OLED初期化
+    u8g2.setContrast(1);
+
+    u8g2.clearBuffer();
+    u8g2.setFont(u8g2_font_crox1hb_tf);
+    u8g2.drawStr(0, 17, "Hello world");
+    u8g2.sendBuffer();
 }
 
 void Loop() {
-    robot.motor(100, 100);
-    wait(1);
-    robot.motor(0, 0);
-    wait(1);
 }
